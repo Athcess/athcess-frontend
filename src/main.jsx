b5180b2cfc82c3b50cafaf6e3fa2dddc3c789f37
Pann Sengpanich <panncraftyo@gmail.com>
2024-03-14 14:05:53 +0700

    create folder structure, basic components (not completed)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import styles from "./scss/index.module.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { MantineProvider } from "@mantine/core";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <GoogleOAuthProvider clientId="">
+        <MantineProvider>
+          <App></App>
+          <ReactQueryDevtools />
+        </MantineProvider>
+      </GoogleOAuthProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
