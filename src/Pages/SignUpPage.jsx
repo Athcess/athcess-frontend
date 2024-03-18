2101008ee6158873d2cec45e0479bc4bb3bbfc97
Pann Sengpanich <panncraftyo@gmail.com>
2024-03-18 14:04:27 +0700

    progress signup and update mantine dependencies

--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -1,9 +1,117 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useToggle, upperFirst } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { GoogleLogin } from "@react-oauth/google";
 
-export default function SignUpPage() {
+import {
+  TextInput,
+  PasswordInput,
+  Text,
+  Paper,
+  Group,
+  Button,
+  Divider,
+  Checkbox,
+  Anchor,
+  Stack,
+} from "@mantine/core";
+import styles from "../scss/SignUpPage.module.scss";
+import { jwtDecode } from "jwt-decode";
+
+export default function SignUpPage(props) {
+  const navigate = useNavigate();
+  const form = useForm({
+    initialValues: {
+      email: "",
+      name: "",
+      password: "",
+      terms: true,
+    },
+    validate: {
+      email: (val) => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
+      password: (val) =>
+        val.length <= 6
+          ? "Password should include at least 6 characters"
+          : null,
+    },
+  });
+
   return (
-    <div>
-      <h1>Welcome to the Sign Up Page!</h1>
+    <div className={styles.container}>
+      <Paper className={styles.paper} radius="md" p="xl" withBorder {...props}>
+        <Text size="lg" fw={500}>
+          SIGN UP
+        </Text>
+
+        <form onSubmit={form.onSubmit(() => {})}>
+          <Stack>
+            <TextInput
+              required
+              label="Name"
+              placeholder="Your name"
+              value={form.values.name}
+              onChange={(event) =>
+                form.setFieldValue("name", event.currentTarget.value)
+              }
+              radius="md"
+            />
+            <TextInput
+              required
+              label="Email"
+              placeholder="hello@mantine.dev"
+              value={form.values.email}
+              onChange={(event) =>
+                form.setFieldValue("email", event.currentTarget.value)
+              }
+              error={form.errors.email && "Invalid email"}
+              radius="md"
+            />
+            <PasswordInput
+              required
+              label="Password"
+              placeholder="Your password"
+              value={form.values.password}
+              onChange={(event) =>
+                form.setFieldValue("password", event.currentTarget.value)
+              }
+              error={
+                form.errors.password &&
+                "Password should include at least 6 characters"
+              }
+              radius="md"
+            />
+          </Stack>
+          <Group justify="center" mt="xl">
+            <Button type="submit" radius="xl" w="100%">
+              Sign Up
+            </Button>
+          </Group>
+          <Divider label="Or sign up with" labelPosition="center" my="lg" />
+          <Group grow mb="md" mt="md">
+            <GoogleLogin
+              onSuccess={(credentialResponse) => {
+                var credentialResponseDecoded = jwtDecode(
+                  credentialResponse.credential
+                );
+                console.log("Login Success", credentialResponseDecoded);
+                navigate("/home");
+              }}
+              onError={(error) => {
+                console.log("Login Failed", error);
+              }}></GoogleLogin>
+          </Group>
+          <Anchor
+            component="button"
+            type="button"
+            c="dimmed"
+            onClick={() => {}}
+            size="xs"
+            className={styles.anchor}>
+            Already have an account? Login
+          </Anchor>
+        </form>
+      </Paper>
     </div>
   );
 }
