fa8e543e6568b3153813d5faa41570a606f7512e
Pann Sengpanich <panncraftyo@gmail.com>
2024-03-19 22:12:59 +0700

    create skeleton object for home content

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,20 +6,32 @@ import { Grid } from "@mantine/core";
 export default function HomePage() {
   return (
     <div className={styles.container}>
-      <Grid className={styles.grid}>
-        <Grid.Col span={4} className={styles.gridCol}>
-          1
-        </Grid.Col>
-        <Grid.Col span="auto" className={styles.gridCol}>
-          <div className={styles.post}>
-            <div className={styles.postProfile}></div>
-            <div className={styles.postText}></div>
-            <div className={styles.postImage}></div>
-            <div className={styles.postInteraction}></div>
-            <div className={styles.postComment}></div>
-          </div>
-        </Grid.Col>
-      </Grid>
+      <div className={styles.leftContent}>
+        <div className={styles.upcomingEvent}></div>
+      </div>
+      <div className={styles.rightContent}>
+        <div className={styles.post}>
+          <div className={styles.postProfile}></div>
+          <div className={styles.postText}></div>
+          <div className={styles.postImage}></div>
+          <div className={styles.postInteraction}></div>
+          <div className={styles.postComment}></div>
+        </div>
+        <div className={styles.post}>
+          <div className={styles.postProfile}></div>
+          <div className={styles.postText}></div>
+          <div className={styles.postImage}></div>
+          <div className={styles.postInteraction}></div>
+          <div className={styles.postComment}></div>
+        </div>
+        <div className={styles.post}>
+          <div className={styles.postProfile}></div>
+          <div className={styles.postText}></div>
+          <div className={styles.postImage}></div>
+          <div className={styles.postInteraction}></div>
+          <div className={styles.postComment}></div>
+        </div>
+      </div>
     </div>
   );
 }
