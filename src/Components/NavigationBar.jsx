27cf0d239cbbfd3928257c2e9cda853aa2c2f889
Pann Sengpanich <panncraftyo@gmail.com>
2024-03-25 10:42:58 +0700

    implement menu bar

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -8,12 +8,21 @@ import {
   UnstyledButton,
   Image,
   Button,
+  Menu,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { IconSearch } from "@tabler/icons-react";
+import {
+  IconSearch,
+  IconSettings,
+  IconPhoto,
+  IconMessageCircle,
+  IconTrash,
+  IconArrowsLeftRight,
+} from "@tabler/icons-react";
 
 export default function NavigationBar() {
   const [isLogin, setIsLogin] = useState(true);
+  const [menuOpened, setmenuOpened] = useState(false);
   const [opened, { toggle }] = useDisclosure();
   return (
     <header className={styles.container}>
@@ -55,19 +64,72 @@ export default function NavigationBar() {
             size="md"
             radius="xl"
             component="a"
-            href="/signin">
+            href="/signin"
+          >
             Sign in
           </Button>
         </div>
       )}
-
-      <Burger
-        className={styles.burger}
-        color="white"
-        opened={opened}
-        onClick={toggle}
-        aria-label="Toggle navigation"
-      />
+      <Menu offset={20} className={styles.menu}>
+        <Menu.Target>
+          <Burger
+            className={styles.burger}
+            color="white"
+            // opened={opened}
+            onClick={toggle}
+            aria-label="Toggle navigation"
+          />
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item
+            onClick={toggle}
+            leftSection={
+              <Image
+                src="/Images/MenuBar/add_logo.png"
+                style={{ width: rem(32), height: rem(32) }}
+              ></Image>
+            }
+          >
+            Post
+          </Menu.Item>
+          <Menu.Divider />
+          <Menu.Item
+            onClick={toggle}
+            leftSection={
+              <Image
+                src="/Images/MenuBar/friend_logo.png"
+                style={{ width: rem(32), height: rem(32) }}
+              ></Image>
+            }
+          >
+            Friends
+          </Menu.Item>
+          <Menu.Divider />
+          <Menu.Item
+            onClick={toggle}
+            leftSection={
+              <Image
+                src="/Images/MenuBar/body_logo.png"
+                style={{ width: rem(32), height: rem(32) }}
+              ></Image>
+            }
+          >
+            Body Analyzer
+          </Menu.Item>
+          <Menu.Divider />
+          <Menu.Item
+            onClick={toggle}
+            leftSection={
+              <Image
+                src="/Images/MenuBar/logout_logo.png"
+                style={{ width: rem(32), height: rem(32) }}
+              ></Image>
+            }
+          >
+            Logout
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
     </header>
   );
 }
