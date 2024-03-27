db0d065ca5f647fbb14bd8d1b884d4dd539b5720
Pann Sengpanich <panncraftyo@gmail.com>
2024-03-27 09:34:37 +0700

    "about athlete" update

--- src/Components/AthleteProfilePageComponents/AboutAthlete.jsx
+++ src/Components/AthleteProfilePageComponents/AboutAthlete.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { UnstyledButton, rem, Image } from "@mantine/core";
+import { UnstyledButton, rem, Image, Spoiler } from "@mantine/core";
 
 import {
   Chart as ChartJS,
@@ -47,11 +47,10 @@ export default function AboutAthlete() {
               <UnstyledButton className={styles.edit}>
                 <Image
                   src="/Images/AthleteProfilePage/edit_logo.png"
-                  style={{ width: rem(48) }}
-                ></Image>
+                  style={{ width: rem(48) }}></Image>
               </UnstyledButton>
             </div>
-            <div className={styles.radarContainer}>
+            <div className={styles.radarContent}>
               <div className={styles.radar}>
                 <Radar data={data} options={options}></Radar>
               </div>
@@ -59,29 +58,249 @@ export default function AboutAthlete() {
           </div>
           <div className={styles.section}>
             <div className={styles.sectionHeader}>
-              Background{" "}
+              Background
               <UnstyledButton className={styles.edit}>
                 <Image
                   src="/Images/AthleteProfilePage/edit_logo.png"
-                  style={{ width: rem(48) }}
-                ></Image>
+                  style={{ width: rem(48) }}></Image>
               </UnstyledButton>
             </div>
-            <div></div>
-            <div></div>
+            <div className={styles.backgroundContent}>
+              <div className={styles.backgroundElement}>
+                <div className={styles.topic}>
+                  Youth National Basketball Team
+                </div>
+                <div className={styles.topicContent}>
+                  <div className={styles.duration}>
+                    Feb 2023 - Present &nbsp;&nbsp;&nbsp;1 yr 1 mo
+                  </div>
+                  <div className={styles.location}>Bangkok, Thailand</div>
+                  <Spoiler
+                    showLabel="Show more"
+                    hideLabel="Hide"
+                    maxHeight={115}
+                    padding={20}
+                    className={styles.description}>
+                    <ul>
+                      <li>
+                        Hammock chia hexagon fanny pack microdosing swag VHS
+                        authentic. Intelligentsia narwhal crucifix thundercats
+                        master cleanse unicorn. Kale chips cardigan meditation
+                        celiac, yr pok pok narwhal vibecession glossier fanny
+                        pack beard bespoke kinfolk unicorn locavore. Pitchfork
+                        jianbing truffaut narwhal solarpunk.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                    </ul>
+                  </Spoiler>
+                </div>
+              </div>
+              <div className={styles.backgroundElement}>
+                <div className={styles.topic}>
+                  Youth National Basketball Team
+                </div>
+                <div className={styles.topicContent}>
+                  <div className={styles.duration}>
+                    Feb 2023 - Present &nbsp;&nbsp;&nbsp;1 yr 1 mo
+                  </div>
+                  <div className={styles.location}>Bangkok, Thailand</div>
+                  <Spoiler
+                    showLabel="Show more"
+                    hideLabel="Hide"
+                    maxHeight={115}
+                    padding={20}
+                    className={styles.description}>
+                    <ul>
+                      <li>
+                        Hammock chia hexagon fanny pack microdosing swag VHS
+                        authentic. Intelligentsia narwhal crucifix thundercats
+                        master cleanse unicorn. Kale chips cardigan meditation
+                        celiac, yr pok pok narwhal vibecession glossier fanny
+                        pack beard bespoke kinfolk unicorn locavore. Pitchfork
+                        jianbing truffaut narwhal solarpunk.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                    </ul>
+                  </Spoiler>
+                </div>
+              </div>
+            </div>
           </div>
           <div className={styles.section}>
             <div className={styles.sectionHeader}>
-              Achievement{" "}
+              Achievement
               <UnstyledButton className={styles.edit}>
                 <Image
                   src="/Images/AthleteProfilePage/edit_logo.png"
-                  style={{ width: rem(48) }}
-                ></Image>
+                  style={{ width: rem(48) }}></Image>
               </UnstyledButton>
             </div>
-            <div></div>
-            <div></div>
+            <div className={styles.achievementContent}>
+              <div className={styles.achievementElement}>
+                <Image
+                  style={{ width: rem(48) }}
+                  src="/Images/AthleteProfilePage/instu_logo.png"></Image>
+                <div className={styles.detail}>
+                  <div className={styles.topic}>
+                    Assumption College, Thailand
+                  </div>
+                  <div className={styles.subtopic}>
+                    High School Diploma, Mathematics and Sciences
+                  </div>
+                  <div className={styles.date}>Jan 2024</div>
+                  <Spoiler
+                    showLabel="Show more"
+                    hideLabel="Hide"
+                    maxHeight={115}
+                    padding={20}
+                    className={styles.description}>
+                    <ul>
+                      <li>
+                        Hammock chia hexagon fanny pack microdosing swag VHS
+                        authentic. Intelligentsia narwhal crucifix thundercats
+                        master cleanse unicorn. Kale chips cardigan meditation
+                        celiac, yr pok pok narwhal vibecession glossier fanny
+                        pack beard bespoke kinfolk unicorn locavore. Pitchfork
+                        jianbing truffaut narwhal solarpunk.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>{" "}
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>{" "}
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>{" "}
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>{" "}
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                    </ul>
+                  </Spoiler>
+                </div>
+              </div>
+              <div className={styles.achievementElement}>
+                <Image
+                  style={{ width: rem(48) }}
+                  src="/Images/AthleteProfilePage/instu_logo.png"></Image>
+                <div className={styles.detail}>
+                  <div className={styles.topic}>
+                    Assumption College, Thailand
+                  </div>
+                  <div className={styles.subtopic}>
+                    High School Diploma, Mathematics and Sciences
+                  </div>
+                  <div className={styles.date}>Jan 2024</div>
+                  <Spoiler
+                    showLabel="Show more"
+                    hideLabel="Hide"
+                    maxHeight={115}
+                    padding={20}
+                    className={styles.description}>
+                    <ul>
+                      <li>
+                        Hammock chia hexagon fanny pack microdosing swag VHS
+                        authentic. Intelligentsia narwhal crucifix thundercats
+                        master cleanse unicorn. Kale chips cardigan meditation
+                        celiac, yr pok pok narwhal vibecession glossier fanny
+                        pack beard bespoke kinfolk unicorn locavore. Pitchfork
+                        jianbing truffaut narwhal solarpunk.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>{" "}
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>{" "}
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>{" "}
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>{" "}
+                      <li>
+                        Successfully became a member of the national basketball
+                        team in the youth generation.
+                      </li>
+                      <li>Successfully became the main player of the team.</li>
+                    </ul>
+                  </Spoiler>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
