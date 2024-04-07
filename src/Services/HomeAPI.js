f37d4da49844e8622104287d8c35746cd9e4c006
kongwv <116346840+kongwv@users.noreply.github.com>
2024-04-08 04:55:59 +0700

    homeapi

---
+++ src/Services/HomeAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const getPost = aync () => {
+    try {
+        const res = await axios.get('');
+    }catch(error) {
+        console.log(error);
+    }
+}
+
+const getProfilePic = asyn () => {
+    try {
+        const res = await axios.get('');
+    }catch(error) {
+        console.log(error);
+    }
+}
