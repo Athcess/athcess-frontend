fb312e7b775ce1301b2d8722c4f4fbbeeb27e67a
kongwv <116346840+kongwv@users.noreply.github.com>
2024-04-09 05:33:10 +0700

    Update HomeAPI.js

--- src/Services/HomeAPI.js
+++ src/Services/HomeAPI.js
@@ -15,3 +15,32 @@ export const getProfilePic = async () => {
         console.log(error);
     }
 }
+
+export const getComment = async () => {
+    try {
+        const res = await axios.get('');
+    }catch(error) {
+        console.log(error);
+    }
+}
+
+export const postComment = async (e) => {
+    try {
+        const res = await axios.post('', {
+            data : e
+        });
+    }catch(error) {
+        console.log(error);
+    }
+}
+
+export const postPost = async (e) => {
+    try {
+        const res = await axios.post('', {
+            data : e
+        });
+    }catch(error) {
+        console.log(error);
+    }
+}
+
