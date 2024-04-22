779f89c9b14b2a725a735335c9150367a399d33b
Pann Sengpanich <panncraftyo@gmail.com>
2024-04-22 10:05:21 +0700

    Update AboutOrg.jsx

--- src/Components/ProfilePageComponents/OrgProfilePageComponents/AboutOrg.jsx
+++ src/Components/ProfilePageComponents/OrgProfilePageComponents/AboutOrg.jsx
@@ -3,7 +3,7 @@ import { NavLink, useParams } from "react-router-dom";
 import { UnstyledButton, rem, Image, Spoiler } from "@mantine/core";
 import BackgroundElement from "../BackgroundElement";
 import AchievementElement from "../AchievementElement";
-import Cookies from "js-cookie"
+import Cookies from "js-cookie";
 import styles from "../../../scss/ProfilePageComponents/AthleteProfilePageComponents/AboutAthlete.module.scss";
 import { useQuery } from "@tanstack/react-query";
 import { useDisclosure } from "@mantine/hooks";
@@ -14,86 +14,90 @@ export default function AboutOrg() {
   const auth_username = Cookies.get("auth_username");
   const [Backgroundopened, Background] = useDisclosure(false);
   const [Achievementopened, Achievement] = useDisclosure(false);
-  let {username} = useParams()
-  const query = useQuery({ queryKey: ["profile", username], queryFn: () => profileAthlete(username) });
+  let { username } = useParams();
+  const query = useQuery({
+    queryKey: ["profile", username],
+    queryFn: () => profileAthlete(username),
+  });
   if (query.status === "success") {
     const Achievements = query.data.data.achievements;
     const Backgrounds = query.data.data.experiences;
-  return (
-    
-    <div className={styles.container}>
-    <EditBackgroundModal
-      opened={Backgroundopened}
-      onClose={Background.close}
-      Backgrounds={Backgrounds}
-    />
-    <EditAchievementModal
-      opened={Achievementopened}
-      onClose={Achievement.close}
-      Achievements={Achievements}
-    />
-      <div className={styles.header}>
-        <NavLink className={styles.link} to={`/orgprofile/${username}`}>
-          <UnstyledButton>About</UnstyledButton>
-        </NavLink>
-        {/* <NavLink className={styles.link} to="/orgprofile/post">
+    return (
+      <div className={styles.container}>
+        <EditBackgroundModal
+          opened={Backgroundopened}
+          onClose={Background.close}
+          Backgrounds={Backgrounds}
+        />
+        <EditAchievementModal
+          opened={Achievementopened}
+          onClose={Achievement.close}
+          Achievements={Achievements}
+        />
+        <div className={styles.header}>
+          <NavLink className={styles.link} to={`/orgprofile/${username}`}>
+            <UnstyledButton>About</UnstyledButton>
+          </NavLink>
+          {/* <NavLink className={styles.link} to="/orgprofile/post">
           <UnstyledButton>Post</UnstyledButton>
         </NavLink> */}
-        <NavLink className={styles.link} to={`/orgprofile/${username}/event`}>
-          <UnstyledButton>Event</UnstyledButton>
-        </NavLink>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.section}>
-          <div className={styles.sectionHeader}>
-            Background
-            {username === auth_username && (
-            <UnstyledButton className={styles.edit}>
-              <Image
-                src="/Images/ProfilePage/editSection_logo.png"
-                style={{ width: rem(48) }}></Image>
-            </UnstyledButton>
-            )}
+          <NavLink className={styles.link} to={`/orgprofile/${username}/event`}>
+            <UnstyledButton>Event</UnstyledButton>
+          </NavLink>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.section}>
+            <div className={styles.sectionHeader}>
+              Background
+              {username === auth_username && (
+                <UnstyledButton
+                  className={styles.edit}
+                  onClick={Background.open}>
+                  <Image
+                    src="/Images/ProfilePage/editSection_logo.png"
+                    style={{ width: rem(48) }}></Image>
+                </UnstyledButton>
+              )}
+            </div>
+            <div className={styles.backgroundContent}>
+              {Backgrounds?.map((e) => {
+                return (
+                  <>
+                    <BackgroundElement
+                      key={e.experience_id}
+                      data={e}></BackgroundElement>
+                  </>
+                );
+              })}
+            </div>
           </div>
-          <div className={styles.backgroundContent}>
-                {Backgrounds?.map((e) => {
-                  return (
-                    <>
-                      <BackgroundElement
-                        key={e.experience_id}
-                        data={e}
-                      ></BackgroundElement>
-                    </>
-                  );
-                })}
+          <div className={styles.section}>
+            <div className={styles.sectionHeader}>
+              Achievement
+              {username === auth_username && (
+                <UnstyledButton
+                  className={styles.edit}
+                  onClick={Achievement.open}>
+                  <Image
+                    src="/Images/ProfilePage/editSection_logo.png"
+                    style={{ width: rem(48) }}></Image>
+                </UnstyledButton>
+              )}
+            </div>
+            <div className={styles.achievementContent}>
+              {Achievements?.map((e) => {
+                return (
+                  <>
+                    <AchievementElement
+                      key={e.ahievement_id}
+                      data={e}></AchievementElement>
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
-        <div className={styles.section}>
-          <div className={styles.sectionHeader}>
-            Achievement
-            {username === auth_username && (
-            <UnstyledButton className={styles.edit}>
-              <Image
-                src="/Images/ProfilePage/editSection_logo.png"
-                style={{ width: rem(48) }}></Image>
-            </UnstyledButton>
-            )}
-          </div>
-          <div className={styles.achievementContent}>
-                {Achievements?.map((e) => {
-                  return (
-                    <>
-                      <AchievementElement
-                        key={e.ahievement_id}
-                        data={e}
-                      ></AchievementElement>
-                    </>
-                  );
-                })}
-              </div>
-        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
